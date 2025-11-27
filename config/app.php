63c430d6f767833db51ec466d3d09848146f4a23
alwood <oleklisalwood@proton.me>
2025-11-27 19:43:20 +0100

    feat: Add encryption service by integrating `illuminate/encryption` and configuring `app.php`.

--- config/app.php
+++ config/app.php
@@ -44,6 +44,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Encryption Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    */
+
+    'key' => env('APP_KEY'),
+
+    'cipher' => 'AES-256-CBC',
+
+    /*
+    |--------------------------------------------------------------------------
     | Autoloaded Service Providers
     |--------------------------------------------------------------------------
     |
@@ -55,6 +70,7 @@ return [
 
     'providers' => [
         App\Providers\AppServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
     ],
 
 ];
